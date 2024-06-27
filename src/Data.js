96c8621749c4e14a60562eafeda7d10eea54e5b3
hellgate-coder <hellgate.vishesh25@gmail.com>
2024-06-27 15:26:20 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,74 @@
+export const UserData = [
+  {
+    id: 1,
+    year: "Jan",
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: "Feb",
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: "Mar",
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: "Apr",
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: "May",
+    userGain: 51551,
+    userLost: 6662,
+  },
+  {
+    id: 6,
+    year: "Jun",
+    userGain: 69156,
+    userLost: 5552,
+  },
+  {
+    id: 7,
+    year: "july",
+    userGain: 73995,
+    userLost: 3362,
+  },
+  {
+    id: 8,
+    year: "Aug",
+    userGain: 82233,
+    userLost: 2342,
+  },
+  {
+    id: 9,
+    year: "Sep",
+    userGain: 91122,
+    userLost: 2300,
+  },
+  {
+    id: 10,
+    year: "oct",
+    userGain: 43070,
+    userLost: 2999,
+  },
+  {
+    id: 11,
+    year: "Nov",
+    userGain: 79333,
+    userLost: 2340,
+  },
+  {
+    id: 12,
+    year: "Dec",
+    userGain: 90133,
+    userLost: 7884,
+  },
+];
